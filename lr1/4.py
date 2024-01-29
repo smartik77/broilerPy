2edb296f9c33e86024cb71c1dc7f851f5d2a6f61
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-01-29 19:41:17 +0300

    Update 4.py

--- lr1/4.py
+++ lr1/4.py
@@ -1,6 +1,6 @@
 # C + D
-
-from math import pi
+from math import pi, fabs
+from random import uniform
 
 def sIsTrue(r1, r2):
     s = pi * r2**2
@@ -10,23 +10,31 @@ def sIsTrue(r1, r2):
     s -= (pi * r1**2) / 4  #  + D слева
     return s
 
-def monteCarlo(r2, n):
-    points_inside = 0
-    total_points = 0
+def monteCarlo(r1, r2, n):
+    pointIsHit = 0
 
     for i in range(n):
-        x = random.uniform(-radius, radius)
-        y = random.uniform(-radius, radius)
+        x = uniform(-r2, r2)
+        y = uniform(-r2, r2)
+        
+        if (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y > 0):  #  D справа
+            pointIsHit += 1
+        elif (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y < 0):  #  С
+            pointIsHit += 1
+        elif (x**2 + y**2 > r1**2 and x < 0 and y < 0 and fabs(x) < r2 and fabs(y) < r2):  #  D слева
+            pointIsHit += 1
 
-        if is_point_inside_circle(x, y, radius):
-            points_inside += 1
+    pointIsHit += 1
+    sSquare = r2**2 * 4
 
-        total_points += 1
-
-    return (points_inside / total_points) * (4 * radius * radius)
+    return (pointIsHit / n) * sSquare
 
 r1 = 10
 r2 = r1 * 2
 
 s = sIsTrue(r1, r2)
 
+while (True):
+    n = int(input("Введите количество точек N: "))
+    print(f"Метод Монте-Карло: {monteCarlo(r1, r2, n)} кв. ед.")
+    print(f"Точное значение: {s} кв. ед.\n")

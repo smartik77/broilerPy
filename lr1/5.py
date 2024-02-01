2a49ac63aa5d3e3da1f347c3838e729d3db2c9c8
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-01 13:27:12 +0300

    Update 5.py

--- lr1/5.py
+++ lr1/5.py
@@ -1,4 +1,4 @@
-import random
+from random import uniform
 from math import log, sin, fabs, tan
 
 def f1(x):
@@ -7,18 +7,34 @@ def f1(x):
 def f2(x):
     return fabs(tan(0.2 * x)) + x
 
+def monteCarlo(a, b, n):
+    pointIsHit = 0
+    side = b - a
+    
+    for i in range(n):
+        x = uniform(-side / 2, side / 2)
+        y = uniform(-side / 2, side / 2)
+        
+        if (x >= a and y >= f1(y) and y <= f2(y)):
+            pointIsHit += 1
+
+    sSquare = side**2
+
+    return (pointIsHit / n) * sSquare
+
+
 while (True):
     variant = int(input("Для выбора метода интегрирования введите число(1 - метод прямоугольников, 2 - метод трапеций, 0 - выход): "))
     a = float(input("Введите нижний предел интегрирования а: "))
     b = float(input("Введите верхний предел интегрирования b: "))
-    dx = float(input("Введите шаг интегрирования dx: \n"))
-    # n = int(input("Введите количество точек N: "))
+    dx = float(input("Введите шаг интегрирования dx: "))
+    n = int(input("Введите количество точек N: \n"))
 
     x = a
-    result = None
+    s = 0
 
     if (variant == 1):  #  метод прямоугольников
-        s = 0
+
         while (x <= b):
             y1 = f1(x)
             y2 = f2(x)
@@ -27,34 +43,28 @@ while (True):
             ds = y * dx
             s += ds
             x += dx
-
-        result = s
-        print(result)
+        print(s)
 
     elif (variant == 2):  #  метод трапеций
-        s = (fabs(f1(a) - f2(a)) + fabs(f1(b) - f2(b))) / 2
 
-        n = 10  #  количество разрезов
-
-        h = (b - a) / n
-
-        while (n != 0):
+        while (x <= b):
             y1 = f1(x)
+            y1Next = f1(x + dx)
+            
             y2 = f2(x)
-            y = fabs(y1 - y2)
-
-            ds = y * slices
-            s += ds
-            x += slices
-
-        s *= slices
-        result = s
-        print(result)
+            y2Next = f2(x + dx)
+            
+            ds1 = ((y1  + y1Next) / 2) * dx
+            ds2 = ((y2 + y2Next) / 2) * dx
+            s += fabs(ds1 - ds2)
+            x += dx
+        print(s)
 
     elif (variant == 0):
         break
     else:
         continue
-
-
-
+    
+    print(monteCarlo(a, b, n))
+    
+    

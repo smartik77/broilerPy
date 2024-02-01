30ced8988f7653e761108826daccdf114c924a1c
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-01 20:27:07 +0300

    Update 5.py

--- lr1/5.py
+++ lr1/5.py
@@ -9,26 +9,47 @@ def f2(x):
 
 def monteCarlo(a, b, n):
     pointIsHit = 0
-    side = b - a
-    
+
+    width = b - a
+    hight = findMaxY(dx, a, b) - findMinY(dx, a, b)
+     
     for i in range(n):
-        x = uniform(-side / 2, side / 2)
-        y = uniform(-side / 2, side / 2)
+        x = uniform(a, b)
+        y = uniform(findMinY(dx, a, b), findMaxY(dx, a, b))
         
-        if (x >= a and y >= f1(y) and y <= f2(y)):
+        if (x >= a and y >= f1(y) and y <= f2(y) and x <= b):
             pointIsHit += 1
 
-    sSquare = side**2
+    sSquare = width * hight
 
     return (pointIsHit / n) * sSquare
 
+def findMinY(dx, a, b):
+    minY = f1(a)
+    y = a
+    while (y <= b):
+        if (minY > f1(y + dx)):
+            minY = f1(y + dx)
+
+        y += dx
+    return minY
+
+def findMaxY(dx, a, b):
+    maxY = f2(a)
+    y = a
+    while (y <= b):
+        if (maxY < f2(y + dx)):
+            maxY = f2(y + dx)
+
+        y += dx
+    return maxY
 
 while (True):
     variant = int(input("Для выбора метода интегрирования введите число(1 - метод прямоугольников, 2 - метод трапеций, 0 - выход): "))
     a = float(input("Введите нижний предел интегрирования а: "))
     b = float(input("Введите верхний предел интегрирования b: "))
     dx = float(input("Введите шаг интегрирования dx: "))
-    n = int(input("Введите количество точек N: \n"))
+    n = int(input("Введите количество точек N: "))
 
     x = a
     s = 0
@@ -43,7 +64,7 @@ while (True):
             ds = y * dx
             s += ds
             x += dx
-        print(s)
+        print(f"Метод прямоугольников: {s}")
 
     elif (variant == 2):  #  метод трапеций
 
@@ -58,13 +79,10 @@ while (True):
             ds2 = ((y2 + y2Next) / 2) * dx
             s += fabs(ds1 - ds2)
             x += dx
-        print(s)
+        print(f"Метод трапеций: {s}")
 
     elif (variant == 0):
         break
     else:
         continue
-    
-    print(monteCarlo(a, b, n))
-    
-    
+    print(f"Метод монте-карло: {monteCarlo(a, b, n)}")

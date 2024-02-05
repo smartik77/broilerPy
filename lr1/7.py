75e718ebfc6743d4e0a17b4b3ac507a1c05b04cb
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-05 10:55:35 +0300

    Create 7.py

---
+++ lr1/7.py
@@ -0,0 +1,43 @@
+from math import fabs
+
+def f(x):
+    return (x + 2) / (x**2 - 2 * x - 3)
+    
+while (True):
+    x1 = float(input("Введите значение х1: "))
+    x2 = float(input("Введите значение х2: "))
+   
+    if (fabs(x1) >= 1 or fabs(x2) >= 1):
+        print("Указаны неверные значения х")
+        continue
+    
+    eps = float(input("Введите погрешность е: "))
+
+    if (eps <= 0):
+        print("Указано неверное значени е")
+        continue
+    
+    dx = float(input("Введите шаг dx: "))
+    
+    if (dx <= 0):
+        print("Указано неверное значение dx")
+        continue
+    
+    n = 0
+    resultSum = 0
+    resultLeft = f(x1)
+    
+    print("dх    леваяЧасть    праваяЧасть")
+    while (x1 <= x2):
+        
+        while (eps <= fabs(resultLeft - resultSum)):
+            resultSum += (((-1)**(n + 1) - (5 / 3**(n + 1))) * x1**n) / 4
+    
+            n += 1
+        
+        
+        print(f"{x1:g}    {f(x1):g}    {resultSum:g}")
+        x1 += dx
+    
+
+ 

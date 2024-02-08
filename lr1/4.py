b80a90dda1ed907900941e282768639e1c8ca1df
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-08 10:33:39 +0300

    Update 4.py

--- lr1/4.py
+++ lr1/4.py
@@ -1,27 +1,28 @@
 # C + D
+
 from math import pi, fabs
 from random import uniform
 
-def sIsTrue(r1, r2):
+def exactArea(r1, r2):
     s = pi * r2**2
     s -= pi * r1**2
-    s /= 2  #  D + C справа
+    s /= 2  # D + C справа
     s += r2**2
-    s -= (pi * r1**2) / 4  #  + D слева
+    s -= (pi * r1**2) / 4  # + D слева
     return s
 
 def monteCarlo(r1, r2, n):
-    pointIsHit = 0
+    pointIsHit = 0  # попаданий в область
 
     for i in range(n):
         x = uniform(-r2, r2)
         y = uniform(-r2, r2)
         
-        if (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y > 0):  #  D справа
+        if (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y > 0):  # D справа
             pointIsHit += 1
-        elif (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y < 0):  #  С
+        elif (x**2 + y**2 > r1**2 and x**2 + y**2 < r2**2 and x > 0 and y < 0):  # С
             pointIsHit += 1
-        elif (x**2 + y**2 > r1**2 and x < 0 and y < 0 and fabs(x) < r2 and fabs(y) < r2):  #  D слева
+        elif (x**2 + y**2 > r1**2 and x < 0 and y < 0 and fabs(x) < r2 and fabs(y) < r2):  # D слева
             pointIsHit += 1
 
     pointIsHit += 1
@@ -32,9 +33,9 @@ def monteCarlo(r1, r2, n):
 r1 = 10
 r2 = r1 * 2
 
-s = sIsTrue(r1, r2)
+s = exactArea(r1, r2)
 
 while (True):
     n = int(input("Введите количество точек N: "))
-    print(f"Метод Монте-Карло: {monteCarlo(r1, r2, n)} кв. ед.")
+    print(f"\nМетод Монте-Карло: {monteCarlo(r1, r2, n)} кв. ед.")
     print(f"Точное значение: {s} кв. ед.\n")

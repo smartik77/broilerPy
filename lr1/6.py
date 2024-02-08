469159c3112d1de1692427067b7c095bdeb8b47c
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-08 10:57:22 +0300

    Update 6.py

--- lr1/6.py
+++ lr1/6.py
@@ -1,11 +1,11 @@
 from math import fabs
 
+# левая часть равенства
 def f(x):
     return (x + 2) / (x**2 - 2 * x - 3)
     
 while (True):
     x = float(input("Введите значение х в интервале (-1; 1): "))
-    
     
     if (fabs(x) >= 1):
         print("\nНеверное значение х\n")
@@ -16,12 +16,12 @@ while (True):
     n = 0
     resultSum = 0
     
+    # правая часть неравенства
     while (eps <= (fabs(f(x) - resultSum))):
 
         resultSum += (((-1)**(n + 1) - (5 / 3**(n + 1))) * x**n) / 4
 
         n += 1
 
-   
     print(f"\nЛевая часть равенства = {f(x)}")
     print(f"Правая часть равенства = {resultSum}\n")

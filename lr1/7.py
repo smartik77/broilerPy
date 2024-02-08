ce72e406d2ccdf9ebef6675e031e70ff966767d6
smartik77 <145694891+smartik77@users.noreply.github.com>
2024-02-08 11:06:42 +0300

    Update 7.py

--- lr1/7.py
+++ lr1/7.py
@@ -4,23 +4,23 @@ def f(x):
     return (x + 2) / (x**2 - 2 * x - 3)
     
 while (True):
-    x1 = float(input("Введите значение х1: "))
+    x1 = float(input("\nВведите значение х1: "))
     x2 = float(input("Введите значение х2: "))
    
     if (fabs(x1) >= 1 or fabs(x2) >= 1):
-        print("Указаны неверные значения х")
+        print("\nУказаны неверные значения х\n")
         continue
     
     eps = float(input("Введите погрешность е: "))
 
     if (eps <= 0):
-        print("Указано неверное значени е")
+        print("\nУказано неверное значени е\n")
         continue
     
     dx = float(input("Введите шаг dx: "))
     
     if (dx <= 0):
-        print("Указано неверное значение dx")
+        print("\nУказано неверное значение dx\n")
         continue
     
     n = 0
@@ -32,12 +32,7 @@ while (True):
         
         while (eps <= fabs(resultLeft - resultSum)):
             resultSum += (((-1)**(n + 1) - (5 / 3**(n + 1))) * x1**n) / 4
-    
             n += 1
-        
-        
+
         print(f"{x1:g}    {f(x1):g}    {resultSum:g}")
         x1 += dx
-    
-
- 
